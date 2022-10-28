c256bb80db8db6adb74a01f493761097d6531e2e
CamilingJS <jonathancamiling@gmail.com>
2022-10-28 11:52:18 -0700

    implemented actions within cart and modal

--- starter/src/App.js
+++ starter/src/App.js
@@ -1,4 +1,33 @@
+import { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { calculateTotals } from './features/cart/cartSlice';
+
+
+import Navbar from "./Components/Navbar";
+import CartContainer from "./Components/CartContainer";
+import Modal from "./Components/Modal";
+
 function App() {
-  return <h2>Redux Toolkit</h2>;
+
+  //Cart state
+  const { cartItems } = useSelector((state) => state.cart);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(calculateTotals());
+  }, [cartItems]);
+
+  // Modal state
+  const { isOpen } = useSelector((state) => state.modal);
+
+  return(
+    <main>
+    {isOpen && <Modal /> }  
+    <Navbar />
+    <CartContainer />
+
+    </main>
+  )
+  
+  
 }
 export default App;
